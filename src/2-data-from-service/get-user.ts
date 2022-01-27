05de8b07016c4e8d0a7a0c9a1319d750b31f38c8
ion <utale.ion@gmail.com>
2022-01-27 07:16:03 +0000

    finish example of why typescript is not really type safe

--- src/2-data-from-service/get-user.ts
+++ src/2-data-from-service/get-user.ts
@@ -1,23 +1,25 @@
 import axios from "axios";
 
+const dataUrl = {
+  companies: 'https://raw.githubusercontent.com/ionutale/typescript-introduction-course/main/src/2-data-from-service/source-data/companies.json',
+  user: 'https://raw.githubusercontent.com/ionutale/typescript-introduction-course/main/src/2-data-from-service/source-data/user.json',
+}
 interface iUser {
   name: string,
   age: Number,
   company: string
 }
 
-async function getUser() {
+const getUser = async () => {
   try {
     let user: iUser;
-    let res = await axios.get('./src/2-data-from-service/user.json')
+    let res = await axios.get(dataUrl.companies)
     user = res.data
 
-    console.log("the user is: ", user)
-  } catch (error) {
-    console.log(error)
+    console.log("Data is: ", user)
+  } catch (error: any) {
+    console.log(error.message)
   }
 }
 
-(async () => {
-  await getUser()
-})
+getUser()
